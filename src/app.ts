142f770142f1caafb7d3d4362ba15d9b08caa70f
TinVojnovic <tin.vojnovi1@gmail.com>
2023-11-14 18:12:17 +0100

    Added simple route and connected to mariaDB

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,16 @@
 import fastify from 'fastify'
+import userRoutes from './modules/user/user.routes'
+import { DataSource } from 'typeorm'
+
+
+
+const mariadb = require('mariadb');
+const pool = mariadb.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '123',
+    database: 'test'
+});
 
 const server = fastify()
 
@@ -8,9 +20,15 @@ server.get('/check', async function () {
 
 
 async function main() {
+
+    server.register(userRoutes, { prefix: 'api/users' })
+
     try {
+        let conn = await pool.getConnection();
+
         await server.listen(3000, '0.0.0.0')
         console.log("radi")
+
     } catch (error) {
         console.error(error)
         process.exit(1)
